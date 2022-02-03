56feb9a1c5f72d5aab545427f29616910f32b282
Tiago Ventura <tfj.ventura@gmail.com>
2022-02-03 14:05:39 +0100

    Add Repositories

---
+++ src/main/java/com/example/demo/com/example/demo/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository {
+}
